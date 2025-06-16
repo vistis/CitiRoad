1ca624ec93cf96d366532f3d4320c066929e4678
vistis <visits14@proton.me>
2025-06-16 18:38:50 +0700

    Setup `nuxt-auth-sanctum`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-auth-sanctum'],
+  sanctum: {
+    baseUrl: 'http://localhost:8000'
+  },
 })
