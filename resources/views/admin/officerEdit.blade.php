fb75c59bc94fa19c2740b8f806bee1effac428de
piru168 <piru41914@gmail.com>
2025-07-09 15:49:46 +0700

    Updated Password Form

--- resources/views/admin/officerEdit.blade.php
+++ resources/views/admin/officerEdit.blade.php
@@ -114,16 +114,16 @@
                         </div>
                     </div>
 
-                    <!-- Password (Optional) -->
+                    <!-- New Password (Optional) -->
                     <div>
-                        <label for="password" class="block text-sm font-medium text-gray-700">Password (Leave blank to keep current)</label>
+                        <label for="password" class="block text-sm font-medium text-gray-700"> New Password (Optional)</label>
                         <input type="password" name="password" id="password" }}"
                             class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500">
                     </div>
 
                     <!-- Password Confirmation (Optional) -->
                     <div>
-                        <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm Password</label>
+                        <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm New Password (Optional)</label>
                         <input type="password" name="password_confirmation" id="password_confirmation"
                             class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500">
                     </div>
